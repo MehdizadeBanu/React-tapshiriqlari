1e090cecb2f596886f1126dde3aec465bc7ca084
MehdizadeBanu <71208588+MehdizadeBanu@users.noreply.github.com>
2022-12-23 01:45:27 +0400

    Add files via upload

---
+++ tapshiriq2/src/Address.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Address extends Component {
+  render() {
+    return (
+      <div className="container3">
+        <h2>Address: Reshid Behbudov 53-63</h2>
+      </div>
+    );
+  }
+}
+export default Address;
---
+++ tapshiriq2/src/Name.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Name extends Component {
+  render() {
+    return (
+      <div className="container1">
+        <h2>Name: Banu Mehdizadə</h2>
+      </div>
+    );
+  }
+}
+export default Name;
